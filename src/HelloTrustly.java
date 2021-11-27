829634caff5573dd19c9a12975f3e260140f148c
cotriml <lucascotrim3@hotmail.com>
2021-11-27 16:55:41 -0300

    intial commit

---
+++ src/HelloTrustly.java
@@ -0,0 +1,7 @@
+public class HelloTrustly {
+    public static void main(String args[]) {
+        System.out.println("Name: Lucas Cotrim Machado");
+        System.out.println("Nationality: Brazilian");
+        System.out.println("Be the best at what I do and become a reference in it!");
+    }
+}
